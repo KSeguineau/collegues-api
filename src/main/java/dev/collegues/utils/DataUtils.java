d6259992347359dce8f1bd695bc463b518714c3d
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-09-05 16:40:29 +0200

    spring security

--- src/main/java/dev/collegues/utils/DataUtils.java
+++ src/main/java/dev/collegues/utils/DataUtils.java
@@ -2,6 +2,7 @@ package dev.collegues.utils;
 
 import dev.collegues.entite.Authentification;
 import dev.collegues.entite.Collegue;
+import dev.collegues.exception.CollegueNonTrouveException;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,10 +35,10 @@ public class DataUtils {
     @PostConstruct
     public void createCollegueAuthentification(){
 
-        Collegue c = new Collegue(UUID.randomUUID().toString(), "bob", "bobby", "a@a.fr", LocalDate.now(),
+        Collegue c = new Collegue(UUID.randomUUID().toString(), "admin", "admin", "a@a.fr", LocalDate.now(),
                 "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/180px-SNice.svg.png");
         dataCollegue.put(c.getMatricule(), c);
-        c = new Collegue(UUID.randomUUID().toString(), "tom", "tommy", "b@b.fr", LocalDate.now(),
+        c = new Collegue(UUID.randomUUID().toString(), "user", "user", "b@b.fr", LocalDate.now(),
                 "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/180px-SNice.svg.png");
         dataCollegue.put(c.getMatricule(), c);
         c = new Collegue(UUID.randomUUID().toString(), "math", "mathy", "c@c.fr", LocalDate.now(),
@@ -48,8 +49,8 @@ public class DataUtils {
         dataCollegue.put(c.getMatricule(), c);
 
         dataCollegue.values().forEach(v -> dataAuthentification.add(new Authentification(null,v.getNom(),passwordEncoder.encode(v.getPrenom()),v,Arrays.asList("ROLE_USER"))));
-        dataAuthentification.get(1).setRoles(Arrays.asList("ROLE_ADMIN"));
-        dataAuthentification.get(3).setRoles(Arrays.asList("ROLE_ADMIN"));
+        dataAuthentification.stream().filter(t -> t.getCollegue().getNom().equals("admin")).findFirst().orElseThrow(CollegueNonTrouveException::new).setRoles(Arrays.asList("ROLE_ADMIN"));
+
 
     }
 
