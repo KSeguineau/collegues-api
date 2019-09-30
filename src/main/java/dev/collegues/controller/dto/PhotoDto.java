c8783aace8aa5a4fef743fd3fc7adc6e4c7d7be1
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-09-30 10:37:37 +0200

    photo

--- src/main/java/dev/collegues/controller/dto/PhotoDto.java
+++ src/main/java/dev/collegues/controller/dto/PhotoDto.java
@@ -9,6 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PhotoDto {
 
-    private String matricule;
-    private String Photo;
+	private String matricule;
+	private String photoUrl;
 }
