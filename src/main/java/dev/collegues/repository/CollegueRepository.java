2809869446549cae1321061e71db95b8e4a31794
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-09-30 10:00:27 +0200

    photo

--- src/main/java/dev/collegues/repository/CollegueRepository.java
+++ src/main/java/dev/collegues/repository/CollegueRepository.java
@@ -1,36 +1,43 @@
 package dev.collegues.repository;
 
-import dev.collegues.controller.dto.PhotoDto;
-import dev.collegues.entite.Collegue;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import dev.collegues.controller.dto.PhotoDto;
+import dev.collegues.entite.Collegue;
 
 /**
  * Interface permettant de travailler avec la bdd
  */
-public interface CollegueRepository extends JpaRepository<Collegue,String> {
+public interface CollegueRepository extends JpaRepository<Collegue, String> {
 
-    /**
-     * méthode retournant la liste des collegues ayant le nom recherché
-     * @param nom nom à rechercher
-     * @return une liste de collegues
-     */
-    List<Collegue> findByNom(String nom);
-    List<Collegue> findByEmail(String email);
+	/**
+	 * méthode retournant la liste des collegues ayant le nom recherché
+	 * 
+	 * @param nom
+	 *            nom à rechercher
+	 * @return une liste de collegues
+	 */
+	List<Collegue> findByNom(String nom);
 
-    /**
-     * méthode retournant un collegue selon son matricule
-     * @param matricule matricule à rechercher
-     * @return un collegue
-     */
-    Collegue findByMatricule(String matricule);
+	List<Collegue> findByEmail(String email);
 
-    /**
-     * retourne l’ensemble des photos et matricules des collegues
-     * @return une liste de PhotoDto
-     */
-    @Query("select new PhotoDto(c.matricule,c.photoUrl) from Collegue c")
-    List<PhotoDto> findPhotos();
+	/**
+	 * méthode retournant un collegue selon son matricule
+	 * 
+	 * @param matricule
+	 *            matricule à rechercher
+	 * @return un collegue
+	 */
+	Collegue findByMatricule(String matricule);
+
+	/**
+	 * retourne l’ensemble des photos et matricules des collegues
+	 * 
+	 * @return une liste de PhotoDto
+	 */
+	@Query("select new dev.collegues.controller.dto.PhotoDto(c.matricule,c.photoUrl) from Collegue c")
+	List<PhotoDto> findPhotos();
 }
